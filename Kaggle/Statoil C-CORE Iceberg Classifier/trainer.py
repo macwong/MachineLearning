7f64a68f67799cb2c681384e0c448a7ee3c49848
Dave Mac <ihategates@hotmail.com>
2017-12-03 17:50:22 +1100

    Create a trainer class

---
+++ Kaggle/Statoil C-CORE Iceberg Classifier/trainer.py
@@ -0,0 +1,31 @@
+class Trainer:
+    def __init__(self, models):
+        self.models = models
+        self.predict = []
+        
+    def train(self, batch_size = 32, epoch = 80, saveMe = True):
+        for model in self.models:
+            model.train(batch_size, epoch, saveMe)
+            
+    def predict(self):
+        self.predict = []
+
+        for model in self.models:
+            self.predict.append(model.predict())
+            
+    def submit(self):
+        prediction = pd.DataFrame()
+        
+        for pred in self.predict:
+            prediction += pred
+            
+        prediction = prediction / len(self.predict)
+        
+        submission = pd.DataFrame(test, columns=["id"])
+        
+        submission["is_iceberg"] = prediction
+
+        test_func = lambda p: round(p["is_iceberg"], 4)
+        submission["is_iceberg"] = test_func(submission)
+        submission["is_iceberg"] = submission["is_iceberg"].round(4)
+        submission.to_csv("submission.csv", float_format='%g', index = False)
