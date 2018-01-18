cd3fe2357abe6dbf3d4f1b6db4c29fb8afe575d3
Dave Mac <ihategates@hotmail.com>
2018-01-18 20:08:44 +1100

    Create some static methods

--- AusOpen/davemodel.py
+++ AusOpen/davemodel.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import log_loss
 from sklearn.base import clone
+from sklearn.model_selection import train_test_split
 
 class DaveModelBase:
     __metaclass__ = abc.ABCMeta
@@ -15,20 +16,33 @@ class DaveModelBase:
         self.best_model = clone(self.model)
         
     def train(self):
-        print("Training...")
-        self.best_model.fit(self.X_train, self.y_train)
+        DaveModelBase.train_model(self.best_model, self.X_train, self.y_train)
         
-        pred_train = self.best_model.predict_proba(self.X_train)
+    def train_model(model, X_train, y_train):
+        print("\nTraining...")
+        model.fit(X_train, y_train)
+        
+        pred_train = model.predict_proba(X_train)
         pred_train = np.clip(pred_train, 0.0001, 0.9999)
-        print("Log loss:", log_loss(self.y_train, pred_train))
+        print("Log loss:", log_loss(y_train, pred_train))
         
     def predict(self):
-        print("Predicting...")
-        preds = self.best_model.predict_proba(self.X_test)
-        print(preds.shape)
-        
-        return preds
+        return DaveModelBase.predict_model(self.best_model, self.X_test)
     
+    def predict_model(model, X_test):
+        print("\nPredicting...")
+        preds = model.predict_proba(X_test)
+        print("Prediction count:", preds.shape)
+        
+        return preds       
+#    
+#    def predict()
+#    
+#    def evaluate(data, truth):
+#        X_train, X_test, y_train, y_test = train_test_split(data, truth, test_size = 0.25, random_state = 42)
+#        log_reg_test, pred_test = predict(X_train, y_train, X_test) 
+#        analyse(log_reg_test, X_test, y_test)
+#    
     @abc.abstractmethod
     def get_model(self):
         pass
